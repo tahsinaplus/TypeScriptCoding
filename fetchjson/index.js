866ceb0f86f5aeafa4f3759bc9988dc44fbbfc6e
sarabuntohura <stohura@my.centennialcollege.ca>
2023-07-16 07:42:06 -0400

    Typescript coding

---
+++ fetchjson/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+console.log("Running the program");
+axios_1.default.get(url).then(function (response) {
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    logTodo(id, title, completed);
+});
+var logTodo = function (id, title, completed) {
+    console.log("\n    The Todo with ID: ".concat(id, "\n    has a title of ").concat(title, "\n    is it finished? ").concat(completed, "\n    "));
+};
